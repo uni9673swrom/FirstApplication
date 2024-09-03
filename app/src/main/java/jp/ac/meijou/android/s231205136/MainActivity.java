fbf9c8a72c577ca842a44a860366c3f2cadd95cd
PC_User <231205136@ccmailg.meijo-u.ac.jp>
2024-09-03 12:18:26 +0900

    講義2日目 textの変更, 画像の追加

--- app/src/main/java/jp/ac/meijou/android/s231205136/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s231205136/MainActivity.java
@@ -1,24 +1,38 @@
 package jp.ac.meijou.android.s231205136;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s231205136.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+       binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.gap.setText(R.string.lol);
+        var train = ContextCompat.getDrawable(this, R.drawable.baseline_balcony_24);
+        binding.imageView2.setImageDrawable(train);
     }
 }
