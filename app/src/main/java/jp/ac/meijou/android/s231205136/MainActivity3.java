9e9d2581cd953ff3b88a9d5fef7cbd64b277e08f
PC_User <231205136@ccmailg.meijo-u.ac.jp>
2024-09-04 14:02:00 +0900

    講義三日目 新View作成 画面遷移作成途中

--- app/src/main/java/jp/ac/meijou/android/s231205136/MainActivity3.java
+++ app/src/main/java/jp/ac/meijou/android/s231205136/MainActivity3.java
@@ -8,17 +8,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity3 extends AppCompatActivity {
+import jp.ac.meijou.android.s231205136.databinding.ActivityMainBinding;
 
+public class MainActivity3 extends AppCompatActivity {
+    private ActivityMainBinding binding2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main3);
+        binding2=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding2.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        binding2.button1.setOnClickListener(view ->{
+
         });
     }
 }
