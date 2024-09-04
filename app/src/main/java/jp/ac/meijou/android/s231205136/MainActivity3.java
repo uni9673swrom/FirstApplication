ed347c788fe168f3654612d348ea32be438fd41b
PC_User <231205136@ccmailg.meijo-u.ac.jp>
2024-09-04 16:09:20 +0900

    講義三日目 新別画面にデータを送信、別画面からデータを受け取ることを実装

--- app/src/main/java/jp/ac/meijou/android/s231205136/MainActivity3.java
+++ app/src/main/java/jp/ac/meijou/android/s231205136/MainActivity3.java
@@ -1,30 +1,77 @@
 package jp.ac.meijou.android.s231205136;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.ActivityResultLauncherKt;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Optional;
+
+import jp.ac.meijou.android.s231205136.databinding.ActivityMain3Binding;
 import jp.ac.meijou.android.s231205136.databinding.ActivityMainBinding;
 
 public class MainActivity3 extends AppCompatActivity {
-    private ActivityMainBinding binding2;
+    private ActivityMain3Binding binding;
+    private final ActivityResultLauncher<Intent> getActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result ->{
+                switch (result.getResultCode()) {
+                    case RESULT_OK:
+                        Optional.ofNullable(result.getData())
+                                .map(data -> data.getStringExtra("ret"))
+                                .map(text -> "Result" + text)
+                                .ifPresent(text -> binding.textView4.setText(text));
+                        break;
+                    case RESULT_CANCELED:
+                        binding.textView4.setText("Result: Canceled");
+                        break;
+                    default:
+                        binding.textView4.setText("Result: Unknown(" + result.getResultCode() + ")");
+                        break;
+                }
+            }
+    );
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        binding2=ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding2.getRoot());
+        binding= ActivityMain3Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        binding2.button1.setOnClickListener(view ->{
 
+        binding.button1.setOnClickListener(view ->{
+            var intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        binding.button2.setOnClickListener(view ->{
+            var intent2 = new Intent();
+            intent2.setAction(Intent.ACTION_VIEW);
+            intent2.setData(Uri.parse("https://maplestory.nexon.co.jp"));
+            startActivity(intent2);
+        });
+
+        binding.button.setOnClickListener(view ->{
+            var IE = binding.editTextText2.getText().toString();
+            var intent3 = new Intent(this, MainActivity.class);
+            intent3.putExtra("text",IE);
+            startActivity(intent3);
+        });
+        binding.button5.setOnClickListener(view ->{
+            var intent4 = new Intent(this, MainActivity.class);
+            getActivityResult.launch(intent4);
         });
     }
 }
